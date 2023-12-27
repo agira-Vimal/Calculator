165bd707432d4a07066a68a6efaf34a7629eff19
agira-Vimal <vimal.r@agiratech.com>
2023-12-28 01:29:25 +0530

    Added Functionality Using Javascript Event Handlers

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelectorAll(".digit").forEach(button => {
+    button.addEventListener('click',() =>
+        appendToDisplay(button.value)
+    )
+})
+document.querySelectorAll(".operator").forEach(button => {
+    button.addEventListener('click',() =>
+        appendToDisplay(button.value)
+    )
+})
+document.getElementById("clear").addEventListener("click",()=>{
+document.getElementById("display").value=""
+})
+function appendToDisplay(inputValue){
+    document.getElementById("display").value+= inputValue
+}
+document.getElementById("evaluate").addEventListener("click",()=>{
+    try{
+        let result=eval(document.getElementById("display").value)
+        document.getElementById("display").value=result
+    }
+    catch(error){
+        document.getElementById("display").value="Error"
+    }
+})
